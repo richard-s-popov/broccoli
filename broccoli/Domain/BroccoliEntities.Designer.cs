b736187b68e29e8a1ccc6fe0425d71ede4dad98e
unknown <@.(none)>
2012-12-23 19:27:23 +0600

    fix identity to pool

--- broccoli/Domain/BroccoliEntities.Designer.cs
+++ broccoli/Domain/BroccoliEntities.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'C:\Users\Администратор\Documents\Visual Studio 2012\Projects\Broccoli\Domain\BroccoliEntities.edmx'. 
-// To enable default code generation, change the value of the 'Code Generation Strategy' designer
-// property to an alternate value. This property is available in the Properties Window when the model is
-// open in the designer.
+﻿// Создание кода по умолчанию отключено для модели "C:\work\broccoli\broccoli\Domain\BroccoliEntities.edmx". 
+// Чтобы включить создание кода по умолчанию, замените значение свойства "Стратегия создания кода"
+// конструктора на другое. Это свойство доступно в окне "Свойства", когда модель
+// открыта в конструкторе.
