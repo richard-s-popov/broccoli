b34c34c4f090fd4a630c86ba553e2bb7a7d70f5a
unknown <@.(none)>
2013-03-03 23:59:15 +0600

    dafdsfasdf

---
+++ broccoli/BroccoliMvc/Models/Communications/ContactModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BroccoliTrade.Web.BroccoliMvc.Models.Communications
+{
+    public class ContactModel
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Message { get; set; }
+    }
+}
