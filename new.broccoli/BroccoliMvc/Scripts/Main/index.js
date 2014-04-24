e2643119fda70924db3bcf1f0f62e2bd7b0fad2c
Richard <rick_box@mail.ru>
2014-04-25 00:49:11 +0700

    ыфвафывафыва

--- new.broccoli/BroccoliMvc/Scripts/Main/index.js
+++ new.broccoli/BroccoliMvc/Scripts/Main/index.js
@@ -1,4 +1,6 @@
 ﻿$(document).ready(function () {
+    PammTable();
+
     jQuery(window).bind("load", function () {
         jQuery("div#slider1").codaSlider();
         jQuery("div#slider1").show();
@@ -46,3 +48,73 @@
         format: 'odHM'
     });
 });
+
+function PammTable()
+{
+    $('.blur').blurjs({
+        source: 'body',
+        radius: 10
+    });
+
+    $('#small').spinner({
+        step: 10,
+        min: 10,
+        max: 1499,
+        start: 1000
+    }).on('change', function () {
+        var val = this.value,
+            $this = $(this),
+            max = $this.spinner('option', 'max'),
+            min = $this.spinner('option', 'min');
+        if (!val.match(/^\d+$/)) val = 0; //we want only number, no alpha
+        this.value = val > max ? max : val < min ? min : val;
+    });
+
+    $('#medium').spinner({
+        step: 100,
+        min: 1500,
+        max: 2999,
+        start: 2000
+    }).on('change', function () {
+        var val = this.value,
+            $this = $(this),
+            max = $this.spinner('option', 'max'),
+            min = $this.spinner('option', 'min');
+        if (!val.match(/^\d+$/)) val = 0; //we want only number, no alpha
+        this.value = val > max ? max : val < min ? min : val;
+    });
+
+    $('#large').spinner({
+        step: 100,
+        min: 3000,
+        start: 5000
+    }).on('change', function () {
+        var val = this.value,
+            $this = $(this),
+            max = $this.spinner('option', 'max'),
+            min = $this.spinner('option', 'min');
+        if (!val.match(/^\d+$/)) val = 0; //we want only number, no alpha
+        this.value = val > max ? max : val < min ? min : val;
+    });
+
+    $('.pamm-offer-table tbody td:not(.broker-column)').mouseenter(function () {
+        var index = $(this).index();
+
+        $('.pamm-offer-table tbody tr').each(function () {
+            $('td', this).eq(index).css('background', '#FFF5E5');
+        });
+    }).mouseleave(function () {
+        var index = $(this).index();
+
+        $('.pamm-offer-table tbody tr').each(function () {
+            $('td', this).eq(index).css('background', '#F4F5F7');
+        });
+    });
+
+    $('.small-container .ui-icon.ui-icon-triangle-1-n').click(CalcSmall);
+    $('.small-container .ui-icon.ui-icon-triangle-1-s').click(CalcSmall);
+    $('.small-container .ui-spinner-input').change(CalcSmall);
+    $('.small-container .ui-spinner-input').keyup(CalcSmall);
+
+    CalcSmall();
+}
